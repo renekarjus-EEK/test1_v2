2a1c675ec0059463244da9cd53aa452fa8dad28b
renekarjus-EEK <Qe0\/8@5Bc3s>
2021-12-09 17:48:08 +0200

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/test1_v2/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.test1_v2
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/screens/random_number_screen.dart
@@ -0,0 +1,85 @@
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+class RandomNumberScreen extends StatefulWidget {
+  const RandomNumberScreen({Key? key}) : super(key: key);
+
+  @override
+  _RandomNumberScreenState createState() => _RandomNumberScreenState();
+}
+
+
+//fetch data from API
+Future<http.Response> getRandomNumber() {
+  return http
+      .get(Uri.parse('https://csrng.net/csrng/csrng.php?min=1&max=1000'));
+}
+
+
+//convert response into object
+//create class for random number
+class RandomNumber {
+  final String random;
+
+  RandomNumber({required this.random});
+
+  factory RandomNumber.fromJson(Map<String, dynamic> json) {
+    return RandomNumber(random: json['random']);
+  }
+}
+
+
+//Convert the http.Response to an RandomNumber
+Future<RandomNumber> fetchRandomNumber() async {
+  final response = await http
+      .get(Uri.parse('https://csrng.net/csrng/csrng.php?min=1&max=1000'));
+
+  if (response.statusCode == 200) {
+    // If the server did return a 200 OK response,
+    // then parse the JSON.
+    return RandomNumber.fromJson(jsonDecode(response.body));
+  } else {
+    // If the server did not return a 200 OK response,
+    // then throw an exception.
+    throw Exception('Failed to load random number');
+  }
+}
+
+
+class _RandomNumberScreenState extends State<RandomNumberScreen> {
+
+  //fetch data
+  late Future<RandomNumber> futureRandomNumber;
+
+  @override
+  void initState() {
+    super.initState();
+    futureRandomNumber = fetchRandomNumber();
+  }
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+          ElevatedButton(
+          onPressed: () {},
+          child: const Text('  Get New Random Number   '),
+    ),
+          FutureBuilder<RandomNumber>(
+            future: futureRandomNumber,
+            builder: (context, snapshot) {
+
+            return Text(snapshot.data!.random);
+          }
+
+          ),
+          Text('Previous numbers'),
+    ],
+    ),
+    );
+    }
+  }
